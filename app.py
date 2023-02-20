533791b77025807fee9096da29cc2dec37e5334c
Nushigawa03 <ni.hiroto.03@gmail.com>
2023-02-20 13:08:38 +0900

    MelSpectrogram

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import torchaudio.transforms as T
 import time
 from matplotlib import pyplot as plt
 import numpy as np
-import librosa
 import cv2
 
 
@@ -28,11 +27,9 @@ if "fig" not in st.session_state:
     st.session_state.fig = plt.figure()
 fig = st.session_state.fig
 pitch_indicator = st.pyplot(fig)
-
 if "pitch" not in st.session_state:
     st.session_state.pitch = np.empty(1000)
 pitch = st.session_state.pitch
-
 if "spec" not in st.session_state:
     st.session_state.spec = np.empty((128,1000))
 spec = st.session_state.spec
@@ -57,47 +54,50 @@ while True:
         audio_chunk = np.sum(audio_chunk, axis=1)/len(audio_frame.layout.channels)
         audio = torch.tensor(np.copy(audio_chunk))[None].cuda()
 
-        # tmp = librosa.pyin(audio_chunk,
-        #                 fmin=librosa.note_to_hz('C2'),
-        #                 fmax=librosa.note_to_hz('C7'),
-        #                     )[0]
-        tmp = torchcrepe.predict(audio,
-                                audio_frame.sample_rate,
-                                hop_length = int(audio_frame.sample_rate / 200.),
-                                fmin = 50,
-                                fmax = 1000,
-                                model = 'tiny',
-                                device = 'cuda:0'
-                                )[0].cpu().detach().numpy()
-
+        tmp,periodicity = torchcrepe.predict(
+                audio,
+                audio_frame.sample_rate,
+                hop_length = int(audio_frame.sample_rate / 200.),
+                fmin = 50,
+                fmax = 1000,
+                model = 'tiny',
+                device = 'cuda:0',
+                return_periodicity = True,
+            )
+        periodicity = torchcrepe.filter.median(periodicity, win_length = 3)
+        periodicity = torchcrepe.threshold.Silence(-60.)(
+                periodicity,
+                audio,
+                audio_frame.sample_rate,
+                hop_length = int(audio_frame.sample_rate / 200.),
+            )
+        tmp = torchcrepe.threshold.At(.21)(tmp, periodicity)
+        tmp = torchcrepe.filter.mean(tmp, win_length = 3)
+        
+        tmp = tmp.cpu().detach().numpy()[0]
         f0 = np.zeros_like(tmp)
         f0[tmp > 0] = tmp[tmp > 0]
         pitch = np.append(pitch,f0)[-1000:]
 
         if spectrogram == None:
             spectrogram = T.MelSpectrogram(
-                            sample_rate=audio_frame.sample_rate,
-                            ).cuda()
-        spec_tmp = spectrogram(audio)[0].cpu().detach().numpy()
+                    sample_rate=audio_frame.sample_rate,
+                ).cuda()
+        spec_tmp = spectrogram(audio).cpu().detach().numpy()[0]
         spec_tmp = cv2.resize(spec_tmp, dsize=(tmp.shape[0], 128))
         spec = np.append(spec,spec_tmp,axis=1)[:,-1000:]
 
         fig = plt.figure()
         ax = fig.add_subplot()
-        ax.imshow(spec)
+        ax.imshow(cv2.resize(spec, dsize=(1000, values[1]-values[0])))
         ax.set_ylim(values[0], values[1])
         ax.plot(pitch)
-        print(spec.shape,tmp.shape)
-
-
         pitch_indicator.pyplot(fig)
-
         
         st.session_state.pitch = pitch
         st.session_state.spec = spec
         st.session_state.fig = fig
-
-        
+    
     else:
         status_indicator.write("AudioReciver is not set. Abort.")
         break
