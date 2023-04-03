f34e276a4d0656de279f01c2eba387783ff26131
Nushigawa03 <ni.hiroto.03@gmail.com>
2023-04-03 15:16:31 +0900

    change stun server

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ webrtc_ctx = webrtc_streamer(
         key="speech-to-text",
         mode=WebRtcMode.SENDONLY,
         audio_receiver_size=1024,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.xten.com:3478"]}]},
         media_stream_constraints={"video": False, "audio": True},
     )
 
